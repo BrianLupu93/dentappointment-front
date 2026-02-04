67a2a687f90adbb53d4266171aa9c4ade5d0fb8b
Brian Lupu <brianlupu@gmail.com>
2026-02-04 22:16:10 +0100

    Home init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { useTheme } from "@/context/themeContext";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
+};
+
+export default Home;
