d0f30322359938b2c3a8cdede92b2183509e1d44
Brian Lupu <brianlupu@gmail.com>
2026-02-04 22:16:05 +0100

    Router and context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import PageComponent from "./components/PageComponent";
+import { ThemeProvider } from "./context/themeContext";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Navbar />
+        <PageComponent>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </PageComponent>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
