ebbf1106c784599940715ec7e68ca5fa0d2ddf87
Brian Lupu <brianlupu@gmail.com>
2026-02-04 22:14:49 +0100

    Navbar component initial

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,37 @@
+import { Button } from "./ui/button";
+import { NavLink } from "react-router";
+
+export interface NavLinksProps {
+  isLoggedIn: boolean;
+  mobile?: boolean;
+}
+
+export function NavLinks({ isLoggedIn, mobile = false }: NavLinksProps) {
+  const baseClass = mobile ? "text-lg font-medium" : "text-sm font-medium";
+
+  return (
+    <div className={mobile ? "flex flex-col gap-4" : "flex items-center gap-4"}>
+      <NavLink to='/' className={baseClass}>
+        Home
+      </NavLink>
+
+      {isLoggedIn && (
+        <>
+          <NavLink to='/dashboard' className={baseClass}>
+            Dashboard
+          </NavLink>
+
+          <NavLink to='/services' className={baseClass}>
+            Services
+          </NavLink>
+        </>
+      )}
+
+      {!isLoggedIn ? (
+        <Button onClick={() => {}}>Login</Button>
+      ) : (
+        <Button onClick={() => {}}>Logout</Button>
+      )}
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { TbDental } from "react-icons/tb";
+import ThemeSwitch from "./ThemeSwitch";
+import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
+import { IoMenu } from "react-icons/io5";
+import { NavLinks } from "./NavLink";
+const Navbar = () => {
+  const isLoggedIn = true;
+
+  return (
+    <header className='w-full border-b border-black dark:border-white'>
+      <div className='w-full max-w-5xl mx-auto flex h-16 items-center justify-between px-8 sm:px-16'>
+        {/* Logo */}
+        <TbDental size={30} />
+        {/* Desktop */}
+        <div className='hidden md:flex items-center gap-6'>
+          <ThemeSwitch />
+          <NavLinks isLoggedIn={isLoggedIn} />
+        </div>
+        {/* Mobile */}
+        <div className='md:hidden'>
+          <Sheet>
+            <SheetTrigger asChild>
+              <IoMenu size={30} aria-label='Open menu' />
+            </SheetTrigger>
+            <SheetContent side='right' className='w-64'>
+              <div className='mt-10 flex flex-col gap-6'>
+                <ThemeSwitch />
+                <NavLinks isLoggedIn={isLoggedIn} mobile />
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
