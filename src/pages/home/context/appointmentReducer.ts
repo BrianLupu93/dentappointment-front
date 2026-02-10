208369f7f23eb43d8280e8f8e21c598d05b16973
Brian Lupu <brianlupu@gmail.com>
2026-02-10 23:11:08 +0100

    reducer appointment

--- src/pages/home/context/appointmentReducer.ts
+++ src/pages/home/context/appointmentReducer.ts
@@ -1,4 +1,5 @@
 import type { AppointmentAction } from "./appointmentActions";
+import { initialState } from "./appointmentContext";
 import type { AppointmentState } from "./types";
 
 export function appointmentReducer(
@@ -7,10 +8,37 @@ export function appointmentReducer(
 ): AppointmentState {
   switch (action.type) {
     case "SET_SELECTED_DAY":
+      return { ...state, selectedDay: action.payload };
+
+    case "SET_SERVICES":
+      return { ...state, services: action.payload };
+
+    case "SET_SELECTED_SERVICE":
+      return { ...state, selectedService: action.payload };
+
+    case "SET_SELECTED_START_TIME":
+      return { ...state, selectedStartTime: action.payload };
+
+    case "SET_CALENDAR_AVAILABILITY":
+      return { ...state, calendarAvailability: action.payload };
+
+    case "SET_SERVICE_AVAILABILITY":
+      return { ...state, availableSlots: action.payload };
+
+    case "RESET_APPOINTMENT_STATE":
       return {
         ...state,
-        selectedDay: action.payload,
+        selectedDay: initialState.selectedDay,
+        selectedService: initialState.selectedService,
+        selectedStartTime: initialState.selectedStartTime,
+        availableSlots: initialState.availableSlots,
       };
+
+    case "SET_LOADING":
+      return { ...state, loading: action.payload };
+
+    case "SET_ERROR":
+      return { ...state, error: action.payload };
 
     default:
       return state;
